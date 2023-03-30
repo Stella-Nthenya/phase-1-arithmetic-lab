898d574ef62f4129f76155338642dcfcb8b62bda
Stella Nthenya <stellaalogi@gmail.com>
2023-03-31 01:37:04 +0300

    completed assignment for arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// multiply
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// random number
+const random = Math.floor(Math.random() * 10);
+
+// modulus 4
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+
+// max value
+
+const max = Math.max(8 ,6, 12, 20, 10);
